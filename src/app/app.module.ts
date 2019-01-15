c0aff88b965c8b72521df32dc4ed22155fdd19a4
MarinaGoretskaya <marisha.2307@gmail.com>
2019-01-15 17:34:38 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { Section1Component } from './section1/section1.component';
+import { SectionTopComponentComponent } from './section-top-component/section-top-component.component';
+import { SectionBottomComponentComponent } from './section-bottom-component/section-bottom-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    Section1Component,
+    SectionTopComponentComponent,
+    SectionBottomComponentComponent
   ],
   imports: [
     BrowserModule,
